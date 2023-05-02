2bf89dac5042cc7301cd4868fe73cc779d7d2c69
Thomas <thomas.kleb@ost.ch>
2023-05-02 13:54:50 +0200

    refactor nornir script

--- nornir_magic.py
+++ nornir_magic.py
@@ -5,56 +5,35 @@ from nornir_utils.plugins.functions import print_title, print_result
 
 nr = InitNornir(config_file="config.yaml", dry_run=True)
 
-def vrf_configuration(task):
+def run_configuration_task(task, task_name, template_name):
     r = task.run(task=template_file,
-                 name="VRF Configuration",
-                 template="vrf.j2",
+                 name=task_name,
+                 template=template_name,
                  path=f"templates/")
 
     task.host["config"] = r.result
 
-    task.run(task=napalm_configure,
-             name="Loading VRF Configurations on the device",
-             replace=False,
-             configuration=task.host["config"])
+    if task.host["config"]:
+        task.run(task=napalm_configure,
+                name=f"Loading {task_name} on the device",
+                replace=False,
+                configuration=task.host["config"])
+
+def vrf_configuration(task):
+    run_configuration_task(task, "VRF Configuration", "vrf.j2")
 
 def interface_configuration(task):
-    r = task.run(task=template_file,
-                 name="Interface Configuration",
-                 template="interfaces.j2",
-                 path=f"templates/")
-
-    task.host["config"] = r.result
-
-    if task.host["config"]:
-        task.run(task=napalm_configure,
-                name="Loading Interface Configurations on the device",
-                replace=False,
-                configuration=task.host["config"])
+    run_configuration_task(task, "Interface Configuration", "interfaces.j2")
 
 def bgp_configuration(task):
-    r = task.run(task=template_file,
-                 name="BGP Configuration",
-                 template="bgp.j2",
-                 path=f"templates/")
-
-    task.host["config"] = r.result
-
-    if task.host["config"]:
-        task.run(task=napalm_configure,
-                name="Loading Interface Configurations on the device",
-                replace=False,
-                configuration=task.host["config"])
+    run_configuration_task(task, "BGP Configuration", "bgp.j2")
 
 nr.data.dry_run = False
-print_title("RUNNING VRF CONFIGURATIONS")
-vrf_result = nr.run(task=vrf_configuration)
-print_result(vrf_result)
-print("\n\n")
-print_title("RUNNING INTERFACE CONFIGURATIONS")
-interface_result = nr.run(task=interface_configuration)
-print_result(interface_result)
-print("\n\n")
-print_title("RUNNING BGP CONFIGURATIONS")
-bgp_result = nr.run(task=bgp_configuration)
-print_result(bgp_result)
+
+for task_name, task_func in [("RUNNING VRF CONFIGURATIONS", vrf_configuration),
+                             ("RUNNING INTERFACE CONFIGURATIONS", interface_configuration),
+                             ("RUNNING BGP CONFIGURATIONS", bgp_configuration)]:
+    print_title(task_name)
+    result = nr.run(task=task_func)
+    print_result(result)
+    print("\n\n")
